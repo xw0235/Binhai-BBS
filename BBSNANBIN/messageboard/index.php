2e5633516fd027fa47d595e8e9642a051f862923
FatTig3R <z627229468@qq.com>
2018-11-09 20:44:02 +0800

    增加了无数个bug

--- BBSNANBIN/messageboard/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['username']) && !empty($_SESSION['username'])) {
-    echo "登录成功!";
-} else {
-    echo "你还没有登录，<a href='../login/index.html'>请登录</a>";
-}
-?>
---
+++ BBSNANBIN/user/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: byoji
+ * Date: 2018/11/9
+ * Time: 20:17
+ */
